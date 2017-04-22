3488f41989d82845e3a756b8d12b95820f5ad203
Colin Ashburn <cashburn@purdue.edu>
2017-04-21 21:01:08 -0400

    Completed Insert

--- src/query/Insert.java
+++ src/query/Insert.java
@@ -23,12 +23,14 @@ class Insert implements Plan {
     */
     public Insert(AST_Insert tree) throws QueryException {
         fileName = tree.getFileName();
+        obj = tree.getValues();
 
         //check if the table exists and set schema
         schema = QueryCheck.tableExists(fileName);
+        QueryCheck.insertValues(schema, obj);
         tuple = new Tuple(schema);
 
-        obj = tree.getValues();
+
 
    } // public Insert(AST_Insert tree) throws QueryException
 
@@ -40,10 +42,17 @@ class Insert implements Plan {
 
         tuple.setAllFields(obj);
         HeapFile hf = new HeapFile(fileName);
-        tuple.insertIntoFile(hf);
+        RID rid = tuple.insertIntoFile(hf);
 
         //if indexes exist, add tuple to indexes
+        IndexDesc[] indexes = Minibase.SystemCatalog.getIndexes(fileName);
+        for(int i = 0; i < indexes.length; i++) {
+            HashIndex index = new HashIndex(indexes[i].indexName);
+            SearchKey key = new SearchKey(tuple.getField(indexes[i].columnName));
+            index.insertEntry(key, rid);
+        }
 
+        //increase count in system catalog
         Schema syscatS = Minibase.SystemCatalog.s_rel;
         FileScan syscatFs = new FileScan(syscatS, Minibase.SystemCatalog.f_rel);
         String tmp = null;
@@ -58,7 +67,7 @@ class Insert implements Plan {
         int recCnt = syscatT.getIntFld(1) + 1;
         syscatT.setIntFld(1, recCnt);
         syscatT.insertIntoFile(hf);
-
+        System.out.println("1 rows affected.");
 
     } // public void execute()
 
